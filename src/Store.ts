f5fe232bb4f743720faf777df8dac14c259aa655
Charles Olupot <olupotcharles@gmail.com>
2023-05-04 09:24:49 +0300

    More changes

--- src/Store.ts
+++ src/Store.ts
@@ -1,244 +1,247 @@
 import { domain } from "./Domains";
 import {
-  changeProject,
-  changeFilterBy,
-  changeIndicator,
-  changeIndicatorGroup,
-  changeIndicatorGroupIndex,
-  changeIndicatorGroups,
-  changeIndicatorIndex,
-  changeIndicators,
-  changeOu,
-  changeProgram,
-  changeTrackedEntityType,
-  changePeriod,
-  changeUrl,
-  changeColumns,
-  changeTotal,
-  addRemoveColumn,
-  changeInitialPrograms,
-  changeInitialUnits,
-  changeDataEntryPage,
-  changeProgramEntity,
-  changeInstance,
-  changeOus,
-  changeLevels,
-  changeLevel,
-  addIndicator,
+    changeProject,
+    changeFilterBy,
+    changeIndicator,
+    changeIndicatorGroup,
+    changeIndicatorGroupIndex,
+    changeIndicatorGroups,
+    changeIndicatorIndex,
+    changeIndicators,
+    changeOu,
+    changeProgram,
+    changeTrackedEntityType,
+    changePeriod,
+    changeUrl,
+    changeColumns,
+    changeTotal,
+    addRemoveColumn,
+    changeInitialPrograms,
+    changeInitialUnits,
+    changeDataEntryPage,
+    changeProgramEntity,
+    changeInstance,
+    changeOus,
+    changeLevels,
+    changeLevel,
+    addIndicator,
 } from "./Events";
 import { Store } from "./interfaces";
 
 export const dashboards = domain
-  .createStore<Store>({
-    url: "/",
-    filterBy: "period",
-    indicator: "",
-    ou: "",
-    ous: [],
-    indicatorGroup: "",
-    indicatorGroups: [],
-    program: "vMfIVFcRWlu",
-    trackedEntityType: "KSy4dEvpMWi",
-    indicators: [],
-    period: [{ id: "LAST_3_MONTHS", name: "Last 3 months" }],
-    loading: false,
-    columns: [],
-    organisations: [],
-    programs: [],
-    total: 0,
-    dataEntryPage: "list",
-    programEntity: "KSy4dEvpMWi,vMfIVFcRWlu",
-    instance: "",
-    project: {},
-    level: "",
-    levels: [],
-    descendants: [],
-    indicatorIndex: -1,
-    indicatorGroupIndex: -1,
-  })
-  .on(changeIndicator, (state, indicator: string) => {
-    return { ...state, indicator };
-  })
-  .on(changeIndicatorGroup, (state, indicatorGroup: string | undefined) => {
-    if (indicatorGroup) {
-      return { ...state, indicatorGroup };
-    }
-  })
-  .on(changePeriod, (state, period: any[]) => {
-    return { ...state, period };
-  })
-  .on(changeOu, (state, ou) => {
-    return { ...state, ou };
-  })
-  .on(changeOus, (state, ous) => {
-    return { ...state, ous };
-  })
-  .on(changeUrl, (state, url: string) => {
-    return { ...state, url };
-  })
-  .on(changeFilterBy, (state, filterBy: string) => {
-    return { ...state, filterBy };
-  })
-  .on(changeIndicators, (state, indicators: any[]) => {
-    return { ...state, indicators };
-  })
-  .on(changeIndicatorIndex, (state, indicatorIndex: number) => {
-    return { ...state, indicatorIndex };
-  })
-  .on(changeIndicatorGroupIndex, (state, indicatorGroupIndex: number) => {
-    return { ...state, indicatorGroupIndex };
-  })
-  .on(changeIndicatorGroups, (state, indicatorGroups: any[]) => {
-    return { ...state, indicatorGroups };
-  })
-  .on(changeProject, (state, project) => {
-    return {
-      ...state,
-      project,
-    };
-  })
-  .on(changeProgram, (state, program) => {
-    return {
-      ...state,
-      program,
-    };
-  })
-  .on(changeTrackedEntityType, (state, trackedEntityType) => {
-    return {
-      ...state,
-      trackedEntityType,
-    };
-  })
-  .on(changeColumns, (state, columns) => {
-    return {
-      ...state,
-      columns,
-    };
-  })
-  .on(changeTotal, (state, total) => {
-    return {
-      ...state,
-      total,
-    };
-  })
-  .on(addRemoveColumn, (state, { id, value }) => {
-    const columns = state.columns.map((column: any) => {
-      if (id === column.trackedEntityAttribute.id) {
-        return { ...column, displayInList: value };
-      }
-      return column;
+    .createStore<Store>({
+        url: "/",
+        filterBy: "period",
+        indicator: "",
+        ou: "",
+        ous: [],
+        indicatorGroup: "",
+        indicatorGroups: [],
+        program: "vMfIVFcRWlu",
+        trackedEntityType: "KSy4dEvpMWi",
+        indicators: [],
+        period: [{ id: "LAST_3_MONTHS", name: "Last 3 months" }],
+        loading: false,
+        columns: [],
+        organisations: [],
+        programs: [],
+        total: 0,
+        dataEntryPage: "list",
+        programEntity: "KSy4dEvpMWi,vMfIVFcRWlu",
+        instance: "",
+        project: {},
+        level: "",
+        levels: [],
+        descendants: [],
+        indicatorIndex: -1,
+        indicatorGroupIndex: -1,
+    })
+    .on(changeIndicator, (state, indicator: string) => {
+        return { ...state, indicator };
+    })
+    .on(changeIndicatorGroup, (state, indicatorGroup: string | undefined) => {
+        if (indicatorGroup) {
+            return { ...state, indicatorGroup };
+        }
+    })
+    .on(changePeriod, (state, period: any[]) => {
+        return { ...state, period };
+    })
+    .on(changeOu, (state, ou) => {
+        return { ...state, ou };
+    })
+    .on(changeOus, (state, ous) => {
+        return { ...state, ous };
+    })
+    .on(changeUrl, (state, url: string) => {
+        return { ...state, url };
+    })
+    .on(changeFilterBy, (state, filterBy: string) => {
+        return { ...state, filterBy };
+    })
+    .on(changeIndicators, (state, indicators: any[]) => {
+        return { ...state, indicators };
+    })
+    .on(changeIndicatorIndex, (state, indicatorIndex: number) => {
+        return { ...state, indicatorIndex };
+    })
+    .on(changeIndicatorGroupIndex, (state, indicatorGroupIndex: number) => {
+        return { ...state, indicatorGroupIndex };
+    })
+    .on(changeIndicatorGroups, (state, indicatorGroups: any[]) => {
+        return { ...state, indicatorGroups };
+    })
+    .on(changeProject, (state, project) => {
+        return {
+            ...state,
+            project,
+        };
+    })
+    .on(changeProgram, (state, program) => {
+        return {
+            ...state,
+            program,
+        };
+    })
+    .on(changeTrackedEntityType, (state, trackedEntityType) => {
+        return {
+            ...state,
+            trackedEntityType,
+        };
+    })
+    .on(changeColumns, (state, columns) => {
+        return {
+            ...state,
+            columns,
+        };
+    })
+    .on(changeTotal, (state, total) => {
+        return {
+            ...state,
+            total,
+        };
+    })
+    .on(addRemoveColumn, (state, { id, value }) => {
+        const columns = state.columns.map((column: any) => {
+            if (id === column.trackedEntityAttribute.id) {
+                return { ...column, displayInList: value };
+            }
+            return column;
+        });
+        return {
+            ...state,
+            columns,
+        };
+    })
+    .on(changeInitialPrograms, (state, programs) => {
+        return {
+            ...state,
+            programs,
+        };
+    })
+    .on(changeInitialUnits, (state, organisations) => {
+        return {
+            ...state,
+            organisations,
+        };
+    })
+    .on(changeDataEntryPage, (state, dataEntryPage) => {
+        return {
+            ...state,
+            dataEntryPage,
+        };
+    })
+    .on(changeProgramEntity, (state, programEntity) => {
+        return {
+            ...state,
+            programEntity,
+        };
+    })
+    .on(changeInstance, (state, instance) => {
+        return {
+            ...state,
+            instance,
+        };
+    })
+    .on(changeLevels, (state, levels) => {
+        return {
+            ...state,
+            levels,
+        };
+    })
+    .on(changeLevel, (state, level) => {
+        return {
+            ...state,
+            level,
+        };
     });
-    return {
-      ...state,
-      columns,
-    };
-  })
-  .on(changeInitialPrograms, (state, programs) => {
-    return {
-      ...state,
-      programs,
-    };
-  })
-  .on(changeInitialUnits, (state, organisations) => {
-    return {
-      ...state,
-      organisations,
-    };
-  })
-  .on(changeDataEntryPage, (state, dataEntryPage) => {
-    return {
-      ...state,
-      dataEntryPage,
-    };
-  })
-  .on(changeProgramEntity, (state, programEntity) => {
-    return {
-      ...state,
-      programEntity,
-    };
-  })
-  .on(changeInstance, (state, instance) => {
-    return {
-      ...state,
-      instance,
-    };
-  })
-  .on(changeLevels, (state, levels) => {
-    return {
-      ...state,
-      levels,
-    };
-  })
-  .on(changeLevel, (state, level) => {
-    return {
-      ...state,
-      level,
-    };
-  });
 
 export const orgUnits = dashboards.map((state) => {
-  if (state.level) {
-    return `${state.ous.join(";")};${state.level}`;
-  }
-  return state.ous.join(";");
+    if (state.level) {
+        return `${state.ous.join(";")};${state.level}`;
+    }
+    return state.ous.join(";");
 });
 
 export const maxLevel = dashboards.map((state) => {
-  if (state.ou.length > 0) {
-    const ou: any = state.ou[state.ou.length - 1];
-    return String(ou.path).split("/").length;
-  }
-  return 2;
+    if (state.ou.length > 0) {
+        const ou: any = state.ou[state.ou.length - 1];
+        return String(ou.path).split("/").length;
+    }
+    return 2;
 });
 
 export const periods = dashboards.map((state) => {
-  return state.period.map((p: any) => p.id).join(";");
+    return state.period.map((p: any) => p.id).join(";");
 });
 
 export const allIndicators = dashboards.map((state) => {
-  if (
-    state.indicators &&
-    state.indicatorIndex !== undefined &&
-    state.indicatorIndex !== -1
-  ) {
-    return state.indicators.map((row: any[]) => [
-      row[0],
-      row[state.indicatorIndex],
-    ]);
-  }
-  return [];
+    if (
+        state.indicators &&
+        state.indicatorIndex !== undefined &&
+        state.indicatorIndex !== -1
+    ) {
+        return state.indicators.map((row: any[]) => [
+            row[0],
+            row[state.indicatorIndex],
+        ]);
+    }
+    return [];
 });
 
 export const currentIndicator = dashboards.map((state) => {
-  const indic = state.indicators.find((row: any) => row[0] === state.indicator);
-  if (indic && state.indicatorIndex) {
-    return indic[state.indicatorIndex];
-  }
-  return "";
+    const indic = state.indicators.find(
+        (row: any) => row[0] === state.indicator
+    );
+    if (indic && state.indicatorIndex) {
+        return indic[state.indicatorIndex];
+    }
+    return "";
 });
 
 export const indicatorForGroup = dashboards
-  .map((state) => {
-    if (
-      state.indicators &&
-      state.indicatorGroupIndex &&
-      state.indicatorGroupIndex !== -1
-    ) {
-      const indicators = state.indicators
-        .filter(
-          (row: any) => row[state.indicatorGroupIndex] === state.indicatorGroup
-        )
-        .map((row: any) => [row[0], row[state.indicatorIndex]]);
-      return [...indicators, ["add", "Add new indicator"]];
-    }
-    return [["add", "Add new indicator"]];
-  })
-  .on(addIndicator, (state, indicator) => {
-    return [...state, indicator];
-  });
+    .map((state) => {
+        if (
+            state.indicators &&
+            state.indicatorGroupIndex &&
+            state.indicatorGroupIndex !== -1
+        ) {
+            const indicators = state.indicators
+                .filter(
+                    (row: any) =>
+                        row[state.indicatorGroupIndex] === state.indicatorGroup
+                )
+                .map((row: any) => [row[0], row[state.indicatorIndex]]);
+            return [...indicators, ["add", "Add new indicator"]];
+        }
+        return [["add", "Add new indicator"]];
+    })
+    .on(addIndicator, (state, indicator) => {
+        return [...state, indicator];
+    });
 
 export const $withOptionSet = dashboards.map((state) => {
-  return state.columns
-    .filter((column: any) => column.trackedEntityAttribute.optionSetValue)
-    .map((column: any) => column.trackedEntityAttribute.id);
+    return state.columns
+        .filter((column: any) => column.trackedEntityAttribute.optionSetValue)
+        .map((column: any) => column.trackedEntityAttribute.id);
 });
--- src/components/OrgUnitTreeSelect.tsx
+++ src/components/OrgUnitTreeSelect.tsx
@@ -6,72 +6,72 @@ import { FC } from "react";
 import { db } from "../db";
 
 const OrgUnitTreeSelect: FC<{
-  multiple?: boolean;
-  value: string | string[] | undefined;
-  onChange: (value: string | string[] | undefined) => void;
+    multiple?: boolean;
+    value: string | string[] | undefined;
+    onChange: (value: string | string[] | undefined) => void;
 }> = ({ multiple = false, value, onChange }) => {
-  const organisations = useLiveQuery(() => db.organisations.toArray());
-  const engine = useDataEngine();
-  const onLoadData = async ({ id, children }: any) => {
-    if (children) {
-      return;
-    }
-    try {
-      const {
-        units: { organisationUnits },
-      }: any = await engine.query({
-        units: {
-          resource: "organisationUnits.json",
-          params: {
-            filter: `id:in:[${id}]`,
-            paging: "false",
-            order: "shortName:desc",
-            fields: "children[id,name,path,leaf]",
-          },
-        },
-      });
-      const found = organisationUnits.map((unit: any) => {
-        return unit.children
-          .map((child: any) => {
-            return {
-              id: child.id,
-              pId: id,
-              value: child.id,
-              title: child.name,
-              isLeaf: child.leaf,
-            };
-          })
-          .sort((a: any, b: any) => {
-            if (a.title > b.title) {
-              return 1;
-            }
-            if (a.title < b.title) {
-              return -1;
-            }
-            return 0;
-          });
-      });
-      await db.organisations.bulkPut(flatten(found));
-    } catch (e) {
-      console.log(e);
-    }
-  };
+    const organisations = useLiveQuery(() => db.organisations.toArray());
+    const engine = useDataEngine();
+    const onLoadData = async ({ id, children }: any) => {
+        if (children) {
+            return;
+        }
+        try {
+            const {
+                units: { organisationUnits },
+            }: any = await engine.query({
+                units: {
+                    resource: "organisationUnits.json",
+                    params: {
+                        filter: `id:in:[${id}]`,
+                        paging: "false",
+                        order: "shortName:desc",
+                        fields: "children[id,name,path,leaf]",
+                    },
+                },
+            });
+            const found = organisationUnits.map((unit: any) => {
+                return unit.children
+                    .map((child: any) => {
+                        return {
+                            id: child.id,
+                            pId: id,
+                            value: child.id,
+                            title: child.name,
+                            isLeaf: child.leaf,
+                        };
+                    })
+                    .sort((a: any, b: any) => {
+                        if (a.title > b.title) {
+                            return 1;
+                        }
+                        if (a.title < b.title) {
+                            return -1;
+                        }
+                        return 0;
+                    });
+            });
+            await db.organisations.bulkPut(flatten(found));
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-  return (
-    <TreeSelect<string | string[] | undefined>
-      allowClear={true}
-      treeDataSimpleMode
-      multiple={multiple}
-      style={{ width: "100%" }}
-      value={value}
-      listHeight={700}
-      dropdownStyle={{ overflow: "auto" }}
-      placeholder="Please select organisation unit"
-      onChange={onChange}
-      loadData={onLoadData}
-      treeData={organisations}
-    />
-  );
+    return (
+        <TreeSelect<string | string[] | undefined>
+            allowClear={true}
+            treeDataSimpleMode
+            multiple={multiple}
+            style={{ width: "100%" }}
+            value={value}
+            listHeight={700}
+            dropdownStyle={{ overflow: "auto" }}
+            placeholder="Please select organisation unit"
+            onChange={onChange}
+            loadData={onLoadData}
+            treeData={organisations}
+        />
+    );
 };
 
 export default OrgUnitTreeSelect;
